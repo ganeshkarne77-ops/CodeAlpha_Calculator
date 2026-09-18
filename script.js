e654493c2e7d85c5612c4bd0b4714873c04501ce
ganeshkarne77-ops <ganeshkarne77@gmail.com>
2026-09-19 00:18:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function add(value) {
     document.getElementById("display").value += value;
 }
+function deletelast() {
+    document.getElementById("display").value=document.getElementById("display").value.slice(0,-1);
+
+}
+function positiveNegative() {
+    let display = document.getElementById("display");
+        if (display.value !=="") {
+            display.value=display.value * -1;
+        }
+}
